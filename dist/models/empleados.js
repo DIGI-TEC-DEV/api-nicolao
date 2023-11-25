388007f629f5878ad776f3b870885547cbc9a145
pomaDevOficial <developers.dash00@gmail.com>
2023-11-25 17:34:02 -0500

    cambio empleados model

--- dist/models/empleados.js
+++ dist/models/empleados.js
@@ -5,9 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
 const connection_1 = __importDefault(require("../db/connection"));
-class empleados extends sequelize_1.Model {
+class Empleados extends sequelize_1.Model {
 }
-empleados.init({
+Empleados.init({
     nombre: {
         type: sequelize_1.DataTypes.STRING,
         allowNull: false
@@ -37,7 +37,7 @@ empleados.init({
     }
 }, {
     sequelize: connection_1.default,
-    modelName: 'Empleados',
+    modelName: 'empleados',
     timestamps: false
 });
-exports.default = empleados;
+exports.default = Empleados;
